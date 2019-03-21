ef0755a920e3036709f68ebad75857f5229a3671
Nelson <382352532@qq.com>
2019-03-21 17:33:30 +0800

    good to go init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,221 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <section class="hero is-medium is-primary is-bold is-fullheight">
+      <div class="hero-head">
+        <nav class="navbar is-fixed-top" style="background-color: #42CCAC">
+          <div class="container">
+            <!--<div class="navbar-brand">-->
+            <!--<a class="navbar-item">-->
+            <!--<img-->
+            <!--src="https://bulma.io/images/bulma-type-white.png"-->
+            <!--alt="Logo"-->
+            <!--/>-->
+            <!--</a>-->
+            <!--<span class="navbar-burger burger" data-target="navbarMenuHeroA">-->
+            <!--<span></span>-->
+            <!--<span></span>-->
+            <!--<span></span>-->
+            <!--</span>-->
+            <!--</div>-->
+            <div id="navbarMenuHeroA" class="navbar-menu">
+              <div class="navbar-end">
+                <a class="navbar-item is-active">
+                  教学咨询
+                </a>
+                <a class="navbar-item">
+                  招生报名
+                </a>
+                <a class="navbar-item">
+                  优惠活动
+                </a>
+                <span class="navbar-item">
+                  <a class="button is-primary is-inverted">
+                    <span class="icon">
+                      <i class="fab fa-github"></i>
+                    </span>
+                    <span>点我报名</span>
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">
+            樵英教育机构
+          </h1>
+          <h2 class="subtitle">
+            QiaoYing Educational Institution
+          </h2>
+        </div>
+      </div>
+    </section>
+
+    <section class="section ">
+      <div class="container">
+        <h1 class="title has-text-grey">
+          场地环境
+        </h1>
+        <h2 class="subtitle has-text-grey">
+          Vision Of Our Site
+        </h2>
+        <div class="columns">
+          <div class="column">
+            <figure class="image is-3by5">
+              <img src="https://bulma.io/images/placeholders/480x800.png" />
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-3by5">
+              <img src="https://bulma.io/images/placeholders/480x800.png" />
+            </figure>
+          </div>
+          <div class="column">
+            <figure class="image is-3by5">
+              <img src="https://bulma.io/images/placeholders/480x800.png" />
+            </figure>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section ">
+      <div class="container">
+        <h1 class="title has-text-grey">
+          特色课程
+        </h1>
+        <h2 class="subtitle has-text-grey">
+          Special Course
+        </h2>
+        <div class="columns">
+          <div class="column">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img
+                    src="https://bulma.io/images/placeholders/1280x960.png"
+                    alt="Placeholder image"
+                  />
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img
+                        class="is-rounded"
+                        src="https://bulma.io/images/placeholders/96x96.png"
+                        alt="Placeholder image"
+                      />
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">John Smith</p>
+                    <p class="subtitle is-6">@johnsmith</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                  快乐数学,用生动的图形法将书中枯燥的知识进行解析.既能让孩子更加容易理解,同时也能提高趣味性.
+                  <a>@bulmaio</a>.
+                  <br />
+                  <time datetime="2016-1-1">逢周一晚上 7:00-9:30</time>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img
+                    src="https://bulma.io/images/placeholders/1280x960.png"
+                    alt="Placeholder image"
+                  />
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img
+                        class="is-rounded"
+                        src="https://bulma.io/images/placeholders/96x96.png"
+                        alt="Placeholder image"
+                      />
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">John Smith</p>
+                    <p class="subtitle is-6">@johnsmith</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                  快乐数学,用生动的图形法将书中枯燥的知识进行解析.既能让孩子更加容易理解,同时也能提高趣味性.
+                  <a>@bulmaio</a>.
+                  <br />
+                  <time datetime="2016-1-1">逢周一晚上 7:00-9:30</time>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img
+                    src="https://bulma.io/images/placeholders/1280x960.png"
+                    alt="Placeholder image"
+                  />
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img
+                        class="is-rounded"
+                        src="https://bulma.io/images/placeholders/96x96.png"
+                        alt="Placeholder image"
+                      />
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">John Smith</p>
+                    <p class="subtitle is-6">@johnsmith</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                  快乐数学,用生动的图形法将书中枯燥的知识进行解析.既能让孩子更加容易理解,同时也能提高趣味性.
+                  <a>@bulmaio</a>.
+                  <br />
+                  <time datetime="2016-1-1">逢周一晚上 7:00-9:30</time>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p class="has-text-grey-light">
+          --- @CopyRight 网站一切解析权归樵英教育机构所有 ---
+        </p>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  name: "home"
 };
 </script>
